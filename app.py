d10dbc412421ac7805518d80219dcccef120d9d8
alotlikar1010 <alotlikar98@gmail.com>
2023-11-02 23:33:41 +0530

    image and pdf logic

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
 from ocr_engine import OCREngine
 from utils.utils import allowed_file, save_uploaded_file
+from pdf_extract import extract_text_pdf
+import os
+
+app =Flask(__name__)
+
+app.config['UPLOAD_FOLDER']= 'uploads'
+
+# if upload dir does not exist code
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
+app.route('/',methods =['GET','POST'])
+def index():
+    if request.method =='POST':
+        file = request.files['file']
+
+        file_type = request.form['file-type'] # image or pdf
+
+        if file and allowed_file(file.filename):
+            filename = save_uploaded_file(file)
+            if filename:
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+                extracted_text =""
+
+                if file_type == 'image':
+                    ocr= OCREngine()
+                    extracted_text = ocr.extract_text(file_path)
+                elif file_type == 'pdf':
+                    extracted_text = extract_text_pdf(file_path)
+
+                return render_template('index.html',extracted_text=extracted_text)
+        
+    return render_template('index.html',extracted_text=None)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ pdf_extract.py
@@ -0,0 +1,14 @@
+import filtz
+
+
+def extract_text_pdf(pdf_path):
+    try:
+        pdf_doc = filtz.open(pdf_path)
+        text =""
+
+        for page_num in range(pdf_doc.page_count):
+            page = pdf_doc[page_num]
+            text += page.get_text()
+        return text
+    except Exception as e:
+        return str(e)
